282b63b6c44761ba8a79bef0248b58f800120d1c
zemlianin <detulie@mail.ru>
2023-10-02 22:26:28 +0300

    first commit

---
+++ dubanek.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Установите токен вашего бота
+BOT_TOKEN="6427718017:AAHj_iG7b1FiHJzSo9TzjpwAu-k9xK6-NhM"
+
+# Задайте URL для отправки сообщений в чат
+SEND_MESSAGE_URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
+
+# ID вашего чата, куда бот будет отправлять сообщения
+CHAT_ID="-1001770053832"
+
+#Количество пользователей на момент последнего сообщения
+PREVIOUS_MEMBERS_COUNT=0
+
+# Инициализация переменной для хранения текста последнего сообщения
+LAST_MESSAGE_TEXT=""
+
+# Получение обновлений чата с использованием long polling
+while true; do
+
+  echo start
+
+  # Запрос на получение обновлений
+  UPDATES=$(curl -s "https://api.telegram.org/bot$BOT_TOKEN/getUpdates")
+
+  # Извлечение текста последнего сообщения (если оно есть)
+  LAST_MESSAGE_TEXT=$(echo "$UPDATES" | jq -r '.result[-1].message.text')
+
+  echo checking
+
+  # Проверка наличия новых сообщений и их текста
+  if [[ "$UPDATES" != '{"ok":true,"result":[]}' && -n "$LAST_MESSAGE_TEXT" ]]; then
+
+    echo $LAST_MESSAGE_TEXT
+
+	# Отправляем запрос к API Telegram для получения информации о количестве участников 
+    CURRENT_MEMBERS_COUNT=$(curl -s "https://api.telegram.org/bot$BOT_TOKEN/getChatMembersCount?chat_id=$CHAT_ID" | jq -r '.result')
+
+	echo $CURRENT_MEMBERS_COUNT
+
+	if [ "$CURRENT_MEMBERS_COUNT" -gt "$PREVIOUS_MEMBERS_COUNT" ]; then
+
+      # Если количество участников увеличилось, то кто-то новый присоединился
+      NEW_MEMBERS_COUNT=$((CURRENT_MEMBERS_COUNT - PREVIOUS_MEMBERS_COUNT))
+      echo "$NEW_MEMBERS_COUNT new members joined the chat"
+	  
+	  # Отправка приветствия
+      MESSAGE="Привет! Расскажи свой любимый анекдот)"
+      curl -s -X POST "$SEND_MESSAGE_URL" -d "chat_id=$CHAT_ID" -d "text=$MESSAGE"
+
+      # Обновляем значение PREVIOUS_MEMBERS_COUNT
+      PREVIOUS_MEMBERS_COUNT="$CURRENT_MEMBERS_COUNT"
+    fi
+
+	PREVIOUS_MEMBERS_COUNT=$CURRENT_MEMBERS_COUNT
+    # Если текст последнего сообщения содержит оповещение о добавлении нового участника
+    # if [[ "$LAST_MESSAGE_TEXT" == *"new_chat_participant"* ]]; then
+	
+	#  echo it is new participant
+
+    #   # Отправка приветственного сообщения
+    #   MESSAGE="Hello! Tell your favorite joke"
+    #   curl -s -X POST "$SEND_MESSAGE_URL" -d "chat_id=$CHAT_ID" -d "text=$MESSAGE"
+
+    #   # Очистка обновлений
+    #   curl -s "https://api.telegram.org/bot$BOT_TOKEN/getUpdates" > /dev/null
+    # fi
+
+  fi
+
+  # Пауза между проверками (например, раз в минуту)
+  sleep 1
+done
