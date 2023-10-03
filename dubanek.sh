91f540e3bf06bc655505fe4158b2fc6656353580
zemlianin <detulie@mail.ru>
2023-10-03 19:17:47 +0300

    delete-api-token

--- dubanek.sh
+++ dubanek.sh
@@ -10,7 +10,7 @@ SEND_MESSAGE_URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
 CHAT_ID="-1001770053832"
 
 # API ключ
-API_KEY="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"
+API_KEY=""
 
 # Текст, который вы хотите передать в качестве запроса
 REQUEST_TEXT="Расскажи утренний анекдот на случайную тему"
@@ -37,9 +37,8 @@ generate_gpt_response() {
        "model": "gpt-3.5-turbo",
        "temperature": 0.9,
        "messages": [{"role": "user", "content": "'"$request_text"'"}],
-       "max_tokens": 35
+       "max_tokens": 50
      }')
-  echo $response
   local generated_text=$(echo "$response" | jq -r '.choices[0].message.content')
   echo "$generated_text"
 }
@@ -51,7 +50,6 @@ handle_new_members() {
     local new_members_count=$((current_members_count - PREVIOUS_MEMBERS_COUNT))
     local message="Привет! Расскажи свой любимый анекдот)"
     send_message "$message"
-    echo "new member"
     PREVIOUS_MEMBERS_COUNT="$current_members_count"
   fi
 }
@@ -66,47 +64,45 @@ send_morning_joke() {
 # Основной цикл для получения обновлений чата
 while true; do
   echo "start"
-  
+
   # Запрос на получение обновлений
   updates=$(curl -s "https://api.telegram.org/bot$BOT_TOKEN/getUpdates")
-  
+
   # Извлечение текста последнего сообщения (если оно есть)
   LAST_MESSAGE_TEXT=$(echo "$updates" | jq -r '.result[-1].message.text')
 
   echo "checking"
-  
+
   # Проверка наличия новых сообщений и их текста
   if [[ "$updates" != '{"ok":true,"result":[]}' && -n "$LAST_MESSAGE_TEXT" ]]; then
     echo "$LAST_MESSAGE_TEXT"
-  
-    # Отправляем запрос к API Telegram для получения информации о количестве участников 
+
+    # Отправляем запрос к API Telegram для получения информации о количестве участников
     CURRENT_MEMBERS_COUNT=$(curl -s "https://api.telegram.org/bot$BOT_TOKEN/getChatMembersCount?chat_id=$CHAT_ID" | jq -r '.result')
-  
+
     echo "$CURRENT_MEMBERS_COUNT"
-  
+
     handle_new_members "$CURRENT_MEMBERS_COUNT"
     PREVIOUS_MEMBERS_COUNT="$CURRENT_MEMBERS_COUNT"
   fi
-  
   # Получаем текущее время в формате часы:минуты
   CURRENT_TIME=$(date "+%H:%M")
   CURRENT_DATE=$(date +"%Y-%m-%d")
-  
+
   # Время, с которым мы сравниваем (например, 11:00)
   COMPARISON_TIME="11:00"
-  
+
   # Проверяем, является ли текущее время 11:00
   if [[ "$CURRENT_DATE" != "$PREVIOUS_DATA" && "$CURRENT_TIME" > "$COMPARISON_TIME" ]]; then
     REQUEST_TEXT="Очень короткий анекдот на случайную тему на 10 слов"
     GENERATED_TEXT=$(generate_gpt_response "$REQUEST_TEXT")
-    echo "$GENERATED_TEXT"
-  
+
     send_morning_joke "$GENERATED_TEXT"
-  
+
     # Установка новой даты последней отправки
     PREVIOUS_DATA="$CURRENT_DATE"
   fi
-  
+
   # Пауза между проверками
   sleep 5
 done
